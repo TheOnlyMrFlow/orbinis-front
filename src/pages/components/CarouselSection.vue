ef81fd32a3b0ad5e42bb02cc55bc2f2cfc9d0917
MrFLow <comte.florian@gmail.com>
2020-07-17 19:48:13 +0200

    initial commit

---
+++ src/pages/components/CarouselSection.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="section" id="carousel">
+    <div class="container" style="max-width: 100%;">
+      <div class="row justify-content-center">
+        <div class="col-12">
+          <el-carousel height="500px" arrow.height="2000px">
+            <el-carousel-item>
+              <parallax
+                class="page-header-image d-block"
+                style="background-image: url('img/company-parallax.jpg')"
+              >
+              </parallax>
+              <!-- <img class="d-block" src="img/bg1.jpg" alt="First slide" /> -->
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Nature, United States</h5>
+              </div>
+            </el-carousel-item>
+            <el-carousel-item>
+              <parallax
+                class="page-header-image d-block"
+                style="background-image: url('img/bg3.jpg')"
+              >
+              </parallax>
+              <!-- <img class="d-block" src="img/bg3.jpg" alt="Second slide" /> -->
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Somewhere Beyond, United States</h5>
+              </div>
+            </el-carousel-item>
+            <el-carousel-item>
+              <!-- <img class="d-block" src="img/bg4.jpg" alt="Third slide" /> -->
+              <parallax
+                class="page-header-image d-block"
+                style="background-image: url('img/bg4.jpg')"
+              >
+              </parallax>
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Yellowstone National Park, United States</h5>
+              </div>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { Carousel, CarouselItem } from 'element-ui';
+
+export default {
+  components: {
+    [Carousel.name]: Carousel,
+    [CarouselItem.name]: CarouselItem
+  }
+};
+</script>
+<style></style>
---
+++ src/pages/components/ExamplesSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="section section-examples" data-background-color="black">
+    <div class="space-50"></div>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <router-link to="/company" target="_blank">
+            <img src="img/landing.jpg" alt="Image" class="img-raised" />
+          </router-link>
+          <router-link
+            to="/company"
+            class="btn btn-simple btn-primary btn-round"
+            >View Company Page
+          </router-link>
+        </div>
+        <div class="col">
+          <router-link to="/profile" target="_blank">
+            <img src="img/profile.jpg" alt="Image" class="img-raised" />
+          </router-link>
+          <router-link
+            to="/profile"
+            class="btn btn-simple btn-primary btn-round"
+            >View Profile Page</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
